1fa4ce6b0a1ec6fa4c597bf88cf06ce778131c86
--replace-all <sorgente711@gmail.com>
2024-07-09 13:43:26 -0400

    Start modularizing

--- src/neural_net.py
+++ src/neural_net.py
@@ -19,89 +19,101 @@ class LayerTorch:
 class NNWeightsTorch:
     layers: List[LayerTorch]
 
+class NeuralNetwork:
 
-def relu_torch(x: torch.Tensor) -> torch.Tensor:
-    return torch.maximum(x, torch.zeros_like(x))
+    def __init__(self, network: NNWeightsTorch):
+        self.network = network
+        self.eval = False
+
+    def relu_torch(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.maximum(x, torch.zeros_like(x))
+
+    def gelu(sekf, x: torch.Tensor) -> torch.Tensor:
+        return 0.5 * x * (1 + math.tanh(math.sqrt(2 / math.pi) * (x + 0.0044715 * x ** 3)))
 
 
-def softmax_torch(x: torch.Tensor) -> torch.Tensor:
-    x = x - torch.max(x, dim=1, keepdim=True).values
-    exps = torch.exp(x)
-    denominators = torch.sum(exps, dim=1, keepdim=True)
-    return exps / denominators
+    def softmax_torch(self, x: torch.Tensor) -> torch.Tensor:
+        x = x - torch.max(x, dim=1, keepdim=True).values
+        exps = torch.exp(x)
+        denominators = torch.sum(exps, dim=1, keepdim=True)
+        return exps / denominators
 
 
-def compute_nn_pytorch(
-    xs: torch.Tensor, network: NNWeightsTorch, training: bool
-) -> Tuple[
-    List[torch.Tensor],
-    List[torch.Tensor],
-    List[Optional[torch.Tensor]],
-    List[Optional[float]],
-    List[Optional[float]],
-]:
-    # print(f"l1 Biases: {network.layers[-1].biases}")
-    # print(f"l0 Weights: {network.layers[0].weights}")
-    activations: List[torch.Tensor] = [xs]
-    last_layer_outputs: torch.Tensor = xs  # Two dimensional
-    zs: List[torch.Tensor] = []
-    preact_list: List[Optional[Tuple[torch.Tensor, torch.Tensor]]] = []
-    batch_means: List[Optional[float]] = []
-    batch_vars: List[Optional[float]] = []
-    for layer_num, layer in enumerate(network.layers):
-        z: torch.Tensor = (
-            last_layer_outputs @ layer.weights.T + layer.biases
-        )  # type:ignore
+    def compute_batch_norm(self, z: torch.Tensor, running_mean: Optional[float], running_var: Optional[float],
+                           bn_gain: torch.Tensor, bn_bias: torch.Tensor) -> torch.Tensor:
+        if not self.eval:
+            bn_mean = (1.0 / z.shape[0]) * (z.sum(0, keepdim=True))
+            bn_diff = z - bn_mean
+            bn_diff_sq = bn_diff ** 2
+            bn_var = (1.0 / z.shape[0]) * bn_diff_sq.sum(
+                0, keepdim=True
+            )  # 1/m  * sum(xi-xhat)^2
+            bn_var_2 = (1.0 / (z.shape[0] - 1)) * bn_diff_sq.sum(0, keepdim=True)
 
-        if layer.batch_norm is not None:
-            if training:
-                bn_mean = (1.0 / z.shape[0]) * (z.sum(0, keepdim=True))
-                bn_diff = z - bn_mean
-                bn_diff_sq = bn_diff**2
-                bn_var = (1.0 / z.shape[0]) * bn_diff_sq.sum(
-                    0, keepdim=True
-                )  # 1/m  * sum(xi-xhat)^2
-                bn_var_2 = (1.0 / (z.shape[0] - 1)) * bn_diff_sq.sum(0, keepdim=True)
+        else:
+            assert running_mean is not None
+            assert running_var is not None
+            bn_mean = running_mean
+            bn_var = running_var
+            bn_var_2 = running_var
+            bn_diff = z - bn_mean
 
-            else:
-                bn_mean = layer.running_mean
-                bn_var = layer.running_var
-                bn_var_2 = layer.running_var
-                bn_diff = z - bn_mean
-
-            bn_var_inv = (bn_var + 1e-5) ** -0.5  # This is 1/sqrt(var + epsilon)
-            x_hat = bn_diff * bn_var_inv
-            preact = x_hat * layer.batch_norm[0] + layer.batch_norm[1]
-            hpreact_fast = (
-                layer.batch_norm[0]
+        bn_var_inv = (bn_var + 1e-5) ** -0.5  # This is 1/sqrt(var + epsilon)
+        x_hat = bn_diff * bn_var_inv
+        preact = (
+                bn_gain
                 * (z - z.mean(0, keepdim=True))
                 / torch.sqrt(z.var(0, keepdim=True, unbiased=True) + 1e-5)
-                + layer.batch_norm[1]
-            )
-            torch.allclose(hpreact_fast, preact)
-
-            preact_list.append((bn_var_inv, x_hat))
-            batch_means.append(bn_mean)
-            batch_vars.append(bn_var_2)
-        else:
-            preact = z
-            preact_list.append(None)
-            batch_means.append(None)
-            batch_vars.append(None)
-        zs.append(preact)
-        next_layer_outputs = (
-            relu_torch(preact)
-            if layer_num != len(network.layers) - 1
-            else softmax_torch(preact)
+                + bn_bias
         )
-        # print(f"Next layer: {next_layer_outputs}")
-        activations.append(next_layer_outputs)
-        last_layer_outputs = next_layer_outputs
-    return zs, activations, preact_list, batch_means, batch_vars
+        return (preact, bn_var_inv, x_hat, bn_mean, bn_var_2)
+        # preact_list.append((bn_var_inv, x_hat))
+        # batch_means.append(bn_mean)
+        # batch_vars.append(bn_var_2)
+
+    def compute_nn_pytorch(
+        self, xs: torch.Tensor
+    ) -> Tuple[
+        List[torch.Tensor],
+        List[torch.Tensor],
+        List[Optional[torch.Tensor]],
+        List[Optional[float]],
+        List[Optional[float]],
+    ]:
+        # print(f"l1 Biases: {network.layers[-1].biases}")
+        # print(f"l0 Weights: {network.layers[0].weights}")
+        activations: List[torch.Tensor] = [xs]
+        last_layer_outputs: torch.Tensor = xs  # Two dimensional
+        zs: List[torch.Tensor] = []
+        preact_list: List[Optional[Tuple[torch.Tensor, torch.Tensor]]] = []
+        batch_means: List[Optional[float]] = []
+        batch_vars: List[Optional[float]] = []
+        for layer_num, layer in enumerate(self.network.layers):
+            z: torch.Tensor = (
+                    last_layer_outputs @ layer.weights.T + layer.biases
+            )  # type:ignore
+
+            if layer.batch_norm is not None:
+                pass
+            else:
+                preact = z
+                preact_list.append(None)
+                batch_means.append(None)
+                batch_vars.append(None)
+            zs.append(preact)
+            next_layer_outputs = (
+                self.relu_torch(preact)
+                if layer_num != len(self.network.layers) - 1
+                else self.softmax_torch(preact)
+            )
+            # print(f"Next layer: {next_layer_outputs}")
+            activations.append(next_layer_outputs)
+            last_layer_outputs = next_layer_outputs
+        return zs, activations, preact_list, batch_means, batch_vars
 
 
 def random_weights_nn(
-    data_size: int, layer_sizes: List[Tuple[int, bool]], device: str, seed: Optional[int] = None
+        data_size: int, layer_sizes: List[Tuple[int, bool]], device: str, seed: Optional[int] = None
 ) -> NNWeightsTorch:
     if seed is not None:
         torch.manual_seed(seed)
